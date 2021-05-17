42f5421917f3f519528efc3bf6cae3d26a8cb76c
z4the <83844748+z4the@users.noreply.github.com>
2021-05-17 18:39:44 -0500

    Update DecodeCommand.java

--- src/main/java/bot/commands/DecodeCommand.java
+++ src/main/java/bot/commands/DecodeCommand.java
@@ -4,12 +4,19 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
+import bot.MorseDecoder.MorseDecoder;
 
 public class DecodeCommand implements Command {
     public void process(MessageReceivedEvent event, List<String> args) throws Exception {
         MessageChannel channel = event.getChannel();
-        //long milli = System.currentTimeMillis() - event.getMessage().getCreationTime().toInstant().toEpochMilli();
+        String toConvert = "";
 
-        channel.sendMessage("bruh").queue();
+        for (String word : args) {
+            toConvert = (toConvert + word );
+        }
+        //long milli = System.currentTimeMillis() - event.getMessage().getCreationTime().toInstant().toEpochMilli();
+        MorseDecoder decode = new MorseDecoder();
+        String output = decode.MorseDecoder(toConvert);
+        channel.sendMessage(output).queue();
     }
 }
